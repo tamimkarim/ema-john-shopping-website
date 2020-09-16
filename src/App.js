adc9a106c9c97f983139d6393ddb8dea0466a9e8
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-09-16 17:31:38 +0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
-
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
+
   return (
     <div>
-     <Header></Header>
-     <Shop></Shop>
+      <Header></Header>
+      <Router>
+        <Switch>
+          <Route path ="/shop">
+          <Shop></Shop>
+          </Route>
+          <Route path ="/review">
+            <Review></Review>
+          </Route>
+          <Route path ="/inventory">
+            <Inventory></Inventory>
+          </Route>
+
+
+        </Switch>
+
+      </Router>
+     
     </div>
   );
 }
